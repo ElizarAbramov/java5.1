7640e62f184c40dcb24738474cb1afc84c10d833
ElizarAbramov <88653410+ElizarAbramov@users.noreply.github.com>
2021-10-22 20:56:48 +0700

    Update SQRServiceTest.java

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -10,9 +10,7 @@ public class SQRServiceTest {
     @ParameterizedTest
     @CsvSource(
             value = {"from two hundred to three hundred,200,300,3",
-                     "from two hundred to five hundred,200,500,8"
-            }
-    )
+                     "from two hundred to five hundred,200,500,8"})
     void shouldCalculateNumberOfSquares(String testName,int min, int max, int expected) {
         SQRService sqrService = new SQRService();
 
@@ -20,5 +18,4 @@ public class SQRServiceTest {
 
         assertEquals(expected, actual);
     }
-
 }
